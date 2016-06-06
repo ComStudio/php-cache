33ddbbfda888f7d2bd1ebacba80a8eb7afcb607b
KsaR <KsaR99@users.noreply.github.com>
2016-06-06 06:26:40 +0200

    Update and rename .gitignore to cache.php
    Initial commit...

---
+++ cache.php
@@ -0,0 +1,15 @@
+<?php
+require './class.Cache.php';
+
+use KsaR\Components\Cache;
+
+$cache = new Cache('cache/', '.cache', true);
+
+if ($cache->valid('number', 30)) { // cache has up to 30s ?
+    echo 'Cached: ', $cache->get('number'); // if yes, display.
+} else { // else, save new.
+    $html = '<p>Welcome ! Lucky number is: '.random_int(0, 100).'.</p>';
+
+    echo $html;
+    $cache->set('number', $html);
+}
