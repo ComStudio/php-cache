55cedd4f53aae64d2ae3339a9350910595056745
KsaR <KsaR99@users.noreply.github.com>
2016-06-06 06:29:29 +0200

    Update cache.php

--- cache.php
+++ cache.php
@@ -7,7 +7,7 @@ $cache = new Cache('cache/', '.cache', true);
 
 if ($cache->valid('number', 30)) { // cache has up to 30s ?
     echo 'Cached: ', $cache->get('number'); // if yes, display.
-} else { // else, save new.
+} else { // else, insert new.
     $html = '<p>Welcome ! Lucky number is: '.random_int(0, 100).'.</p>';
 
     echo $html;
