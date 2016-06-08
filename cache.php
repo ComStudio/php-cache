af0bbddcfb4d6a0a1b6eefb79f51c4984198d3d1
KsaR <KsaR99@users.noreply.github.com>
2016-06-08 18:31:11 +0200

    Update cache.php
    Fix PSR.

--- cache.php
+++ cache.php
@@ -1,4 +1,16 @@
 <?php
+/**
+* Cache system
+*
+* PHP version 7
+*
+* @category Cache
+* @package  KsaR_Components
+* @author   KsaR
+* @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+* @link     https://github.com/KsaR99/php-cache
+*
+*/
 require './class.Cache.php';
 
 use KsaR\Components\Cache;
@@ -7,7 +19,7 @@ $cache = new Cache('cache/', '.cache', true);
 
 if ($cache->valid('number', 30)) { // cache has up to 30s ?
     echo $cache->get('number'); // if yes, display.
-} else { // else, insert new.
+} else { // else, save new.
     $html = '<p>Welcome ! Lucky number is: '.random_int(0, 100).'.</p>';
 
     echo $html;
